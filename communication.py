7f2966c4ee0445f62f86821a6ac008c693351fc9
unknown <batoncss@yandex.ru>
2022-03-15 11:23:13 +0300

    correction of algorithm errors

--- communication.py
+++ communication.py
@@ -2,9 +2,10 @@ from requests import get
 
 
 def news():
-    page = get('https://vc.ru')
+    page = get('https://vc.ru/popular')
     data = page.text
-    find_start = data.find('<div class="content-header__info">')
+    print(data)
+    find_start = data.find('Статьи редакции')
     url_start = data.find('https://vc.ru/', find_start)
     url_finish = data.find('"', url_start)
     number = ""

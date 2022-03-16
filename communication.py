9ab3aba31dabdaab34cf4b7a48722bf56d4c6d44
batoncss <batoncss@yandex.ru>
2022-03-16 12:44:03 +0300

    add mode 'money'

--- communication.py
+++ communication.py
@@ -1,23 +1,6 @@
 from requests import get
-
-def weather(city_):
-    if city_ == "Spb":
-        site = "https://world-weather.ru/pogoda/russia/saint_petersburg/"
-        city_find = "|Санкт-Петербург|"
-    elif city_ == "Omsk":
-        site = "https://world-weather.ru/pogoda/russia/omsk/"
-        city_find = "|Омск|"
-    else:
-        return "Ваш город недоступен"
-    page = get(site)
-    data = page.text
-    find_start_false = data.find(city_find)
-    find_start = data.find("|", find_start_false + 1, )
-    find_finish = data.find("|", find_start + 1, )
-    number = ""
-    for i in range(find_start + 1, find_finish):
-        number += data[i]
-    return number
+from weather import text_for_weather
+from money import text_for_money
 
 def news():
     page = get('https://vc.ru/popular')
@@ -37,7 +20,9 @@ def text_for_answer(text):
     elif text == 'кек':
         text = 'чебурек'
     elif text == '/weather':
-        text = f'Погода в СПб: {weather("Spb")} градуса по Цельсию'
+        text = text_for_weather()
+    elif text == '/money':
+        text = text_for_money()
     else:
         text = 0
     return text

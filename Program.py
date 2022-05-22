5369d64d43b2944581f8d925c079714b5eb573e6
batoncss <batoncss@yandex.ru>
2022-05-22 22:05:14 +0300

    add error handling

--- Program.py
+++ Program.py
@@ -1,11 +1,15 @@
 from Bot import long_polling, answer
 from communication import text_for_answer
+from error_handling import error_massage
 import configuration
 
 token = configuration.data()
 update_id = 0
 while True:
-    result, update_id = long_polling(token, update_id)
-    chat_id = result[0]['message']['chat']['id']
-    text = result[0]['message']['text']
-    data = answer(token, chat_id, text_for_answer(text))
+    try:
+        result, update_id = long_polling(token, update_id)
+        chat_id = result[0]['message']['chat']['id']
+        text = result[0]['message']['text']
+        data = answer(token, chat_id, text_for_answer(text))
+    except:
+        error_massage()
